5ae6a4f05edba93f6f3fc58d2510f1dd6462c8f9
Meelliee <mariaamengualrigo@gmail.com>
2026-03-06 11:05:20 +0100

    Quiz-MariaAmengualRigo

---
+++ quiz.js
@@ -0,0 +1,69 @@
+ /*Defino el array*/
+        const preguntas = [
+            {
+                imagen: "imagenes/paris.jpg",
+                pregunta: "El puente más antiguo de Paris se llama:",
+                opcion1: "Le pont Neuf" ,
+                opcion2: "Le pont Alexandre III" ,
+                opcion3: "Le pont des Arts" ,
+                opcion4: "Le pont Saint-Louis",
+                correcta: "",
+            },
+            {
+                imagen: "imagenes/pacifico.jpg",
+                pregunta: "¿Cuál es la profundidad promedio del Océano Pacífico?",
+                opcion1: "3,950 metros" ,
+                opcion2: "4,282 metros" ,
+                opcion3: "5,100 metros" ,
+                opcion4: "6,300 metros",
+                correcta: "",
+            },
+            {
+                imagen: "imagenes/cpu.jpg",
+                pregunta: "¿Qué tipo de CPU se caracteriza por un conjunto de instrucciones complejas?",
+                opcion1: "CPU SIMD" ,
+                opcion2: "CPU MIMD" ,
+                opcion3: "CPU CISC" ,
+                opcion4: "CPU RISC",
+                correcta: "",
+            },
+            {
+                imagen: "imagenes/bombilla.jpg",
+                pregunta: "¿Cuánto tiempo seguido estuvo encendida una bombilla, en la primera demostración de su funcionamiento?",
+                opcion1: "La mantuvo encendida 72 horas" ,
+                opcion2: "La mantuvo encendida durante 48 horas seguidas para que todo el mundo viera su utilidad" ,
+                opcion3: "La tuvo encendida 24 horas." ,
+                opcion4: "Le pont Saint-Louis",
+                correcta: "",
+            },
+            {
+                imagen: "imagenes/paris.jpg",
+                pregunta: "El puente más antiguo de Paris se llama:",
+                opcion1: "Le pont Neuf" ,
+                opcion2: "Le pont Alexandre III" ,
+                opcion3: "Le pont des Arts" ,
+                opcion4: "Le pont Saint-Louis",
+                correcta: "",
+            },
+            {
+                imagen: "imagenes/paris.jpg",
+                pregunta: "El puente más antiguo de Paris se llama:",
+                opcion1: "Le pont Neuf" ,
+                opcion2: "Le pont Alexandre III" ,
+                opcion3: "Le pont des Arts" ,
+                opcion4: "Le pont Saint-Louis",
+                correcta: "",
+            }
+        ]
+
+
+
+
+
+
+        /*Páginas donde he encontrado las preguntas:
+        - París: https://pariseando.com/que-tanto-conoces-de-paris/
+        - Pacífico: https://wayground.com/admin/quiz/66085bd89684070740f7f9cd/oceano-pacifico
+        - CPU: https://www.studocu.com/ca-es/quiz/dsbm-tema-3-la-cpu-apuntes-ejemplos/6810078
+        - Bombilla: 
+        */
